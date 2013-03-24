14505dda65d9f56c10f702db261df76dae11a83e
Jezza <jgrevzie@gmail.com>
2013-03-24 14:04:54 +1100

    db_reset.rake removes spaces from input data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,42 +1,126 @@
-Procedure.create(:name => "Coronary Angiography", :abbrev => "C/A", :options => "Setup only, Inject, Non-Inject")
-Procedure.create(:name => "Percutaneious Coronary Intervention", :abbrev => "PCI", :options => "Setup only, Inject, Non-Inject")
-Procedure.create(:name => "Percutaneous transluminal coronay angioplasty", :abbrev => "PTCA", :options => "Setup only, Inject, Non-Inject")
-Procedure.create(:name => "Plain old balloon angioplasty", :abbrev => "POBA", :options => "Setup only, Inject, Non-Inject")
-Procedure.create(:name => "Permanent Pacemaker", :abbrev => "PPM", :options => "Setup only, Assist with Procedure")
-Procedure.create(:name => "Rotablator", :abbrev => "ROTA", :options => "")
-Procedure.create(:name => "Fractional Flow Reserve", :abbrev => "FFR", :options => "")
-Procedure.create(:name => "Intravascular Ultrasound", :abbrev => "IVUS", :options => "")
-Procedure.create(:name => "Optimal Computer Tomography", :abbrev => "OCT", :options => "")
-Procedure.create(:name => "Fliterwire", :abbrev => "FILTER", :options => "")
-Procedure.create(:name => "Thrombectomy Device", :abbrev => "", :options => "")
-Procedure.create(:name => "Intra-Aotic Balloon Pump", :abbrev => "IABP", :options => "")
-Procedure.create(:name => "Pericardiocentesis", :abbrev => "", :options => "")
-Procedure.create(:name => "Bi-Ventricular Pacemaker", :abbrev => "BI V", :options => "")
-Procedure.create(:name => "Automatic Internal Cardiac Defibrillator", :abbrev => "AICD, ICD", :options => "")
-Procedure.create(:name => "Femoral Sheath Removal", :abbrev => "", :options => "")
-Procedure.create(:name => "Radial Sheath Removal", :abbrev => "", :options => "")
-Procedure.create(:name => "Vascular Closure device", :abbrev => "VCD", :options => "")
-Procedure.create(:name => "Renal Denervation", :abbrev => "", :options => "")
-Procedure.create(:name => "Percutaneous Core Valve", :abbrev => "", :options => "")
-Procedure.create(:name => "Trans-Aortic Valve Implantation", :abbrev => "TAVI", :options => "")
-Procedure.create(:name => "Mitral Clip", :abbrev => "", :options => "")
-Procedure.create(:name => "Septal Puncture", :abbrev => "", :options => "")
-Procedure.create(:name => "Structural Heart - Patent Foramen Ovale", :abbrev => "PFO", :options => "")
-Procedure.create(:name => "Structural Heart - Patent Ductus Arteriosus", :abbrev => "PDA", :options => "")
-Procedure.create(:name => "Structural Heart - Atrial Septal Defect", :abbrev => "ASD", :options => "")
-Procedure.create(:name => "Structural Heart - Left Atrial Appendage", :abbrev => "LAA", :options => "")
-Procedure.create(:name => "Valvuloplasty", :abbrev => "", :options => "Mitral, Aortic, Pulmonary, Tricuspid")
-Procedure.create(:name => "Exercise Stress Test", :abbrev => "", :options => "")
-Procedure.create(:name => "Dobutamine Stress Echo", :abbrev => "DSE", :options => "")
-Procedure.create(:name => "Transoesophageal Echogardiography", :abbrev => "TOE", :options => "Bubble Study?")
-Procedure.create(:name => "Elective DCR", :abbrev => "", :options => "")
-Procedure.create(:name => "Temporary  Pacing Wire ", :abbrev => "Temp wire", :options => "")
-Procedure.create(:name => "Emergency Intubation", :abbrev => "", :options => "")
-Procedure.create(:name => "Covered Stent", :abbrev => "", :options => "")
-Procedure.create(:name => "Advanced Life Support", :abbrev => "ALS", :options => "")
-Procedure.create(:name => "Basic Life  Support", :abbrev => "BLS", :options => "")
-Procedure.create(:name => "Intra venous Cannulation", :abbrev => "IVC", :options => "")
-Procedure.create(:name => "Intra venous Therapy", :abbrev => "IVT", :options => "")
-Procedure.create(:name => "Ventricular assist device", :abbrev => "VAD", :options => "")
-Procedure.create(:name => "Interventional Nurses Council", :abbrev => "INC", :options => "")
-Procedure.create(:name => "Femostop device", :abbrev => "Fem stop", :options => "")
+Procedure.create(name: "Coronary Angiography", 
+										abbrev: "C/A", 
+										options: "Setup only,Inject,Non-Inject")
+Procedure.create(name: "Percutaneious Coronary Intervention", 
+										abbrev: "PCI", 
+										options: "Setup only,Inject,Non-Inject")
+Procedure.create(name: "Percutaneous transluminal coronay angioplasty", 
+										abbrev: "PTCA", 
+										options: "Setup only,Inject,Non-Inject")
+Procedure.create(name: "Plain old balloon angioplasty", 
+										abbrev: "POBA", 
+										options: "Setup only,Inject,Non-Inject")
+Procedure.create(name: "Permanent Pacemaker", 
+										abbrev: "PPM", 
+										options: "Setup only,Assist with Procedure")
+Procedure.create(name: "Rotablator", 
+										abbrev: "ROTA", 
+										options: "")
+Procedure.create(name: "Fractional Flow Reserve", 
+										abbrev: "FFR", 
+										options: "")
+Procedure.create(name: "Intravascular Ultrasound", 
+										abbrev: "IVUS", 
+										options: "")
+Procedure.create(name: "Optimal Computer Tomography", 
+										abbrev: "OCT", 
+										options: "")
+Procedure.create(name: "Fliterwire", 
+										abbrev: "FILTER", 
+										options: "")
+Procedure.create(name: "Thrombectomy Device", 
+										abbrev: "", 
+										options: "")
+Procedure.create(name: "Intra-Aotic Balloon Pump", 
+										abbrev: "IABP", 
+										options: "")
+Procedure.create(name: "Pericardiocentesis", 
+										abbrev: "", 
+										options: "")
+Procedure.create(name: "Bi-Ventricular Pacemaker", 
+										abbrev: "BI V", 
+										options: "")
+Procedure.create(name: "Automatic Internal Cardiac Defibrillator", 
+										abbrev: "AICD, ICD", 
+										options: "")
+Procedure.create(name: "Femoral Sheath Removal", 
+										abbrev: "", 
+										options: "")
+Procedure.create(name: "Radial Sheath Removal", 
+										abbrev: "", 
+										options: "")
+Procedure.create(name: "Vascular Closure device", 
+										abbrev: "VCD", 
+										options: "")
+Procedure.create(name: "Renal Denervation", 
+										abbrev: "", 
+										options: "")
+Procedure.create(name: "Percutaneous Core Valve", 
+										abbrev: "", 
+										options: "")
+Procedure.create(name: "Trans-Aortic Valve Implantation", 
+										abbrev: "TAVI", 
+										options: "")
+Procedure.create(name: "Mitral Clip", 
+										abbrev: "", 
+										options: "")
+Procedure.create(name: "Septal Puncture", 
+										abbrev: "", 
+										options: "")
+Procedure.create(name: "Structural Heart - Patent Foramen Ovale", 
+										abbrev: "PFO", 
+										options: "")
+Procedure.create(name: "Structural Heart - Patent Ductus Arteriosus", 
+										abbrev: "PDA", 
+										options: "")
+Procedure.create(name: "Structural Heart - Atrial Septal Defect", 
+										abbrev: "ASD", 
+										options: "")
+Procedure.create(name: "Structural Heart - Left Atrial Appendage", 
+										abbrev: "LAA", 
+										options: "")
+Procedure.create(name: "Valvuloplasty", 
+										abbrev: "", 
+										options: "Mitral,Aortic,Pulmonary,Tricuspid")
+Procedure.create(name: "Exercise Stress Test", 
+										abbrev: "", 
+										options: "")
+Procedure.create(name: "Dobutamine Stress Echo", 
+										abbrev: "DSE", 
+										options: "")
+Procedure.create(name: "Transoesophageal Echogardiography", 
+										abbrev: "TOE", 
+										options: "Bubble Study?")
+Procedure.create(name: "Elective DCR", 
+										abbrev: "", 
+										options: "")
+Procedure.create(name: "Temporary  Pacing Wire", 
+										abbrev: "Temp wire", 
+										options: "")
+Procedure.create(name: "Emergency Intubation", 
+										abbrev: "", 
+										options: "")
+Procedure.create(name: "Covered Stent", 
+										abbrev: "", 
+										options: "")
+Procedure.create(name: "Advanced Life Support", 
+										abbrev: "ALS", 
+										options: "")
+Procedure.create(name: "Basic Life  Support", 
+										abbrev: "BLS", 
+										options: "")
+Procedure.create(name: "Intra venous Cannulation", 
+										abbrev: "IVC", 
+										options: "")
+Procedure.create(name: "Intra venous Therapy", 
+										abbrev: "IVT", 
+										options: "")
+Procedure.create(name: "Ventricular assist device", 
+										abbrev: "VAD", 
+										options: "")
+Procedure.create(name: "Interventional Nurses Council", 
+										abbrev: "INC", 
+										options: "")
+Procedure.create(name: "Femostop device", 
+										abbrev: "Fem stop", 
+										options: "")
--- lib/tasks/db_reset.rake
+++ lib/tasks/db_reset.rake
@@ -7,27 +7,32 @@ require 'fabrication'
 
 DB_DIR = ENV['DIR'] || File.join(Rails.root, 'db')
 
-def clean(s)
-	s ? s.downcase.gsub(/[^a-z]/, '') : ''
-end
+class String
+	def lc_alpha; self ? self.downcase.gsub(/[^a-z]/, '') : '' end
+end		
 
 def load_procs(seeds_file)
-	csv = CSV.foreach(DB_DIR+'/procedures.csv', :headers => true) do |row|
-		create_proc = %Q/Procedure.create(:name => "#{row['name']}", :abbrev => "#{row['abbrev']}", :options => "#{row['options']}")/
+	csv = CSV.foreach(DB_DIR+'/procedures.csv', 
+										headers: true, 
+										converters: lambda {|field| field.nil? ? "" : field}) do |row|
+		p row
+		create_proc = %Q/Procedure.create(name: "#{row['name'].strip}", 
+										abbrev: "#{row['abbrev'].strip}", 
+										options: "#{row['options'].gsub(', ',',').strip}")/
 		seeds_file.puts create_proc
 	end
 end
 def load_nurses
 	csv = CSV.foreach(DB_DIR+'/nurses.csv', headers: true) do |row|
-		row['username'] = clean(row['first_name'][0]) + clean(row['last_name']) unless row['username']
+		row['username'] = 
+			row['first_name'][0].lc_alpha + row['last_name'].lc_alpha unless row['username']
 		row['validator'] = row['validator'] ? row['validator'].downcase.include?('y') : false
 		row['email'] = 
-			"#{clean(row['first_name'])}.#{clean(row['last_name'])}@svpm.org.au" unless row['email']
+			"#{row['first_name'].lc_alpha}.#{row['last_name'].lc_alpha}@svpm.org.au" unless row['email']
 		row['password'] = 'password'
 
 		n = Fabricate :nurse, row.to_hash # CSV::Row needs to be converted to hash
 		50.times { n.completed_procs << Fabricate(:random_completed_proc) }
-
 	end
 end
 
