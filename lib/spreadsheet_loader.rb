dcd39cd6a236c09889525e757e8240550f6981a8
Jezza <jgrevzie@gmail.com>
2013-04-03 01:57:50 +1100

    Started conversion of nurses csv load to xls type spreadsheet.

--- lib/spreadsheet_loader.rb
+++ lib/spreadsheet_loader.rb
@@ -21,37 +21,40 @@ module SpreadsheetLoader
       raise("Couldn't find sheet '#{sheet_name}'")
   end
 
+  def self.get_headers sheet    
+    headers = sheet.first.map {|i| i.downcase}
+    unless headers[0] && headers[0] == "name"
+      raise "Couldn't find start of table (looking for 'n/Name' in column A)"
+    end
+    return headers
+  end    
+
   def self.load_procs file_name
     sheet = load_sheet(file_name, 'procedures')
-    header = sheet.first.map {|i| i.downcase}
-    unless header[0] && header[0] == "name"
-      raise "Couldn't find start of procedure table (looking for 'n/Name' in column A)"
-    end
+    headers = get_headers sheet
 
     sheet.each sheet.first.idx+1 do |row|
-      h = HashWithIndifferentAccess[header.zip row]
+      h = HashWithIndifferentAccess[headers.zip row]
       Fabricate :procedure, name: nil_or_strip(h[:name]),
                             abbrev: nil_or_strip(h[:abbrev]),
                             options: (h[:options].nil? ? nil : h[:options].gsub(', ',',').strip),
                             comments: nil_or_strip(h[:comments])
     end
   end
-  def self.load_nurses_from_spreadsheet(file_name, dept)
-    sheet = load_sheet filename, 'nurses'
-    dept_name = get_dept_name file_name
-    dept1 = Fabricate :dept, name: 'CathLab', hospital: "St V's Private", location: 'Vict'
-    dept2 = Fabricate :dept, name: 'Theatre', hospital: "St V's Private", location: 'Vict'
 
-    csv = CSV.foreach(file_name, headers: true) do |row|
-      fn, ln = row['name'].split[0].lc_alpha, row['name'].split[-1].lc_alpha
-      row['username'] = fn[0] + ln unless row['username']
-      row['validator'] = row['validator'] ? row['validator'].downcase.start_with?('y', 't') : false
-      row['email'] = "#{fn}.#{ln}@svpm.org.au" unless row['email']
-      row['password'] = 'password'
-      
-      # CSV::Row needs to be converted to hash
-      n = Fabricate :nurse, row.to_hash.merge(dept: dept1)
-      50.times { n.completed_procs << Fabricate(:random_completed_proc) }
+  def self.load_nurses_from_spreadsheet(file_name, dept)
+    sheet = load_sheet(file_name, 'nurses')
+    headers = get_headers sheet
+
+    sheet.each sheet.first.idx+1 do |row|
+      h = HashWithIndifferentAccess[headers.zip row]
+      fn, ln = h['name'].split[0].lc_alpha, h['name'].split[-1].lc_alpha
+      h['username'] = fn[0] + ln unless h['username']
+      h['validator'] = h['validator'] ? h['validator'].downcase.start_with?('y', 't') : false
+      h['email'] = "#{fn}.#{ln}@svpm.org.au" unless h['email']
+      h['password'] = 'password'
     end
+      
+    n = Fabricate :nurse, h.merge(dept: dept)
   end
 end
--- spec/lib/spreadsheet_loader_spec.rb
+++ spec/lib/spreadsheet_loader_spec.rb
@@ -6,48 +6,45 @@
 
 
 require 'spec_helper'
-class SpreadsheetLoaderTester
-  include SpreadsheetLoader
-  describe SpreadsheetLoader do
 
-    TEST_XLS = "#{File.dirname __FILE__}/test.xls"
-    SL = SpreadsheetLoader
-    
-    describe "::load_procs", skip_procs: true do
-      before(:each) {SL::load_procs TEST_XLS}
-      def proc_by_name(name) Procedure.where(name: name).first end  
+describe SpreadsheetLoader do
 
-      it "saves correct number of procs"  do
-        Procedure.count.should eq SL::load_sheet(TEST_XLS, 'procedures').row_count-1
-      end
-      it "removes spaces between options" do
-        (proc_by_name "Space between the options").options.should eq "Orange,Red"
-      end
-      it "handles spaces within options, while still removing spaces between options" do
-        (proc_by_name "Options can include spaces").options.should eq "Option 1,Option 2"
-      end
+  TEST_XLS = "#{File.dirname __FILE__}/test.xls"
+  SL = SpreadsheetLoader
+  
+  describe "::load_procs", skip_procs: true do
+    before(:each) {SL.load_procs TEST_XLS}
+    def proc_by_name(name) Procedure.where(name: name).first end 
 
-      before(:each) {@everything = proc_by_name "Everything"}
-      subject {@everything}
-      its(:name) {should eq 'Everything'}
-      its(:abbrev) {should eq 'E'}
-      its(:options) {should eq 'A,B,C,D'}
-      its(:comments) {should eq 'Hello!'}
+    it "saves correct number of procs"  do
+      Procedure.count.should eq SL::load_sheet(TEST_XLS, 'procedures').row_count-1
+    end
+    it "removes spaces between options" do
+      (proc_by_name "Space between the options").options.should eq "Orange,Red"
+    end
+    it "handles spaces within options, while still removing spaces between options" do
+      (proc_by_name "Options can include spaces").options.should eq "Option 1,Option 2"
     end
 
-    describe "::get_dept_name" do
-      specify {expect {get_dept_name("hello")}.to raise_error}
-      specify {SL::get_dept_name("hello.xls").should eq "hello"}
-      specify {SL::get_dept_name("/pretty/long/path/to/hello.xls").should eq "hello"}
-      specify {SL::get_dept_name("with spaces all over.xls").should eq "with spaces all over"}
-      specify {SL::get_dept_name("FunkyCaps.xls").should eq "FunkyCaps"}
-    end
-
-    describe "::load_sheet" do
-      it "finds a sheet called 'procedures' with procs in it" do
-        SL::load_sheet(TEST_XLS, 'procedures').first[0].downcase.should eq 'name'
-      end
-    end  
-
+    subject {proc_by_name "Everything"}
+    its(:name) {should eq 'Everything'}
+    its(:abbrev) {should eq 'E'}
+    its(:options) {should eq 'A,B,C,D'}
+    its(:comments) {should eq 'Hello!'}
   end
+
+  describe "::get_dept_name" do
+    specify {expect {SL.get_dept_name("hello")}.to raise_error}
+    specify {SL.get_dept_name("hello.xls").should eq "hello"}
+    specify {SL.get_dept_name("/pretty/long/path/to/hello.xls").should eq "hello"}
+    specify {SL.get_dept_name("with spaces all over.xls").should eq "with spaces all over"}
+    specify {SL.get_dept_name("FunkyCaps.xls").should eq "FunkyCaps"}
+  end
+
+  describe "::load_sheet" do
+    it "finds a sheet called 'procedures' with procs in it" do
+      SL::load_sheet(TEST_XLS, 'procedures').first[0].downcase.should eq 'name'
+    end
+  end  
+
 end
