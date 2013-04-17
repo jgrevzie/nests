1d6268aae82129f1f3af7373020ef3b5112a1e50
Jezza <jgrevzie@gmail.com>
2013-04-17 10:47:10 +1000

    Sped up nurse home page tests.

--- spec/requests/nurse_home/proc_sections_spec.rb
+++ spec/requests/nurse_home/proc_sections_spec.rb
@@ -8,17 +8,17 @@ shared_examples "a proc section" do
   let(:header_selector) {"h3##{o[:cp_type]}"}
   let(:table_selector) {"table##{o[:cp_type]}"}
 
-  subject { visit_home the_nurse ; find(header_selector).text }
+  subject { visit_home @the_nurse ; find(header_selector).text }
   it { should include o[:header] }
   it { should include (o[:total] || o[:collection].count).to_s }
 
   # Check number of rows to make sure there isn't too much data in the table.
   it "(has table with correct number of rows)" do
-    visit_home the_nurse
+    visit_home @the_nurse
     page.all("#{table_selector} tr").count.should eq o[:collection].count+1
   end
   it "(has table with appropriate data)" do
-    visit_home the_nurse
+    visit_home @the_nurse
     o[:collection].each {|i| find(table_selector).text.should include i.to_s}
   end
   it "(doesn't show table, instead shows some text if there's no rows)" do
@@ -30,27 +30,26 @@ end
 
 shared_examples "a summary section" do |section_name|
   it "(procs that the nurse hasn't done don't appear)" do
-    visit_home the_nurse
-    Fabricate :procedure, name: 'Slice!'
-    find("table##{section_name}").text.should_not include 'Slice'
+    visit_home @the_nurse
+    p = Fabricate :proc_seq
+    find("table##{section_name}").text.should_not include p.name
   end
 end
 
 describe "Nurse home page", reset_db: false do 
-  let!(:the_nurse) do
+  before(:all) do
     reset_db
   
     # This nurse's procs shouldn't appear, unless there's some problem with the code.
-    #(Fabricate :nurse).completed_procs.concat Array.new(10){Fabricate :rand_cp}
+    (Fabricate :nurse).completed_procs.concat Array.new(10){Fabricate :rand_cp}
 
     # Ensure we have at least one of each type of proc
-    (the_nurse=Fabricate :nurse).completed_procs << 
+    (@the_nurse=Fabricate :nurse).completed_procs << 
       Fabricate(:rand_cp, status: CP::VALID, emergency: true)
-    CP::STATUSES.each { |i| the_nurse.completed_procs << Fabricate(:rand_cp, status: i) }
+    CP::STATUSES.each { |i| @the_nurse.completed_procs << Fabricate(:rand_cp, status: i) }
 
     # Plus a bunch of randoms
-    the_nurse.completed_procs.concat Array.new(50){Fabricate :rand_cp}
-    return the_nurse
+    @the_nurse.completed_procs.concat Array.new(50){Fabricate :rand_cp}
   end
 
   def nurse_without status
@@ -65,7 +64,7 @@ describe "Nurse home page", reset_db: false do
     it_behaves_like "a proc section" do      
       let(:o) { {cp_type: 'pending',
        header: 'Procedures awaiting validation',
-       collection: the_nurse.pendings,
+       collection: @the_nurse.pendings,
        owise_nurse: nurse_without(CP::PENDING),
        owise_text: 'Nothing waiting for validation.'} }
      end
@@ -75,30 +74,30 @@ describe "Nurse home page", reset_db: false do
     it_behaves_like "a proc section" do 
       let(:o) { {cp_type: 'rejected',
        header: 'Invalid procedures',
-       collection: the_nurse.rejects,
+       collection: @the_nurse.rejects,
        owise_nurse: nurse_without(CP::REJECTED),
        owise_text: 'No invalid procedures.'} }
     end
     it "has red rows in reject table" do
-      visit_home the_nurse
+      visit_home @the_nurse
       page.should have_css "table#rejected a.rejected"
     end
     it "shows name of rejector in reject table" do
-      visit_home the_nurse
+      visit_home @the_nurse
       name = CP.where(status: CP::REJECTED).first.validated_by.first_name
       page.first("table#rejected td.validated_by").text.should match /#{name}/
     end
     it "when link is clicked, update page should allow proc to be acknowledged" do
-      visit_home the_nurse
+      visit_home @the_nurse
       first("table#rejected a").click
       page.has_css?('input [value="Acknowledge"]').should be_true
     end
   end
 
   def cp_summary(*filter)
-    the_nurse.completed_procs_summary(*filter).map {|name, n| "#{name} #{n}"}
+    @the_nurse.completed_procs_summary(*filter).map {|name, n| "#{name} #{n}"}
   end
-  def cp_total(*filter) the_nurse.completed_procs_total(*filter) end
+  def cp_total(*filter) @the_nurse.completed_procs_total(*filter) end
 
   describe "(validated proc summary) -" do
     it_behaves_like "a proc section" do 
